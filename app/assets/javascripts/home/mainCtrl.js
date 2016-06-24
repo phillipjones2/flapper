d13124ea889c49c12b7afbc203eaf71bbf5a2ee5
Phillip Jones <phillipemersonjones@gmail.com>
2016-06-24 12:19:16 -0500

    Angular app is working in Rails w/o db hook up

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -1,26 +1,25 @@
-
+angular.module('flapperNews')
 .controller('MainCtrl', [
   '$scope',
   'posts',
-  function($scope,posts){
+  function($scope, posts){
     $scope.posts = posts.posts;
 
+    $scope.incrementUpvotes = function(comment) {
+      comment.upvotes += 1;
+    };
     $scope.addPost = function(){
-      if(!$scope.title || $scope.title === ''){return;}
-
+      if(!$scope.title || $scope.title === '') { return; }
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
         upvotes: 0,
         comments: [
-          {author: 'Joe', body: 'Cool posts!', upvotes: 0},
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
           {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
         ]
       });
       $scope.title = '';
       $scope.link = '';
-      };
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
     };
-}]);
+  }])
--- app/assets/javascripts/posts/postsCtrl.js
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -1,17 +1,22 @@
+angular.module('flapperNews')
+.controller('PostsCtrl', [
+  '$scope','$stateParams','posts',
 
-.controller('PostsCtrl',[
-  '$scope',
-  '$stateParams',
-  'posts',
-  $scope.addComment = function(){
-    if($scope.body == ''){return;}
-    $scope.post.comments.push({
-      body: $scope.body,
-      author: 'user',
-      upvotes: 0
-    });
-    $scope.body = '';
-  };
   function($scope, $stateParams, posts){
     $scope.post = posts.posts[$stateParams.id];
-  }]);
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+  }
+]);
