c53be9574ee59e5554294cd8c7d43fd2e6b3988a
Phillip Jones <phillipemersonjones@gmail.com>
2016-06-24 22:10:18 -0500

    functional and auth. next step heroku up

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,14 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!, only: [:create, :upvote]
 
   def create
-    comment = post.comments.create(comment_params)
-    respond_with post, comment
-  end
+      post = Post.find(params[:post_id])
+      comment = post.comments.create(comment_params.merge(user_id: current_user.id))
+      respond_with post, comment
+    end
 
   def upvote
       post = Post.find(params[:post_id])
-      comment = post.comments.find(comment_params.merge(user_id: current_user.id))
+      comment = post.comments.find(params[:id])
       comment.increment!(:upvotes)
 
       respond_with post, comment
